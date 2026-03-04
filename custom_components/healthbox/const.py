ff137be0d23d5837b66f9dd7a87d86f178844473
MrPlanet <the_planetrider@hotmail.com>
2026-03-04 13:39:06 +0100

    Initial commit: Healthbox Home Assistant integration

---
+++ custom_components/healthbox/const.py
@@ -0,0 +1,161 @@
+"""Constants for the Renson Healthbox integration."""
+import voluptuous as vol
+
+from logging import Logger, getLogger
+from datetime import timedelta
+from decimal import Decimal
+
+from homeassistant.const import Platform
+from homeassistant.helpers import config_validation as cv
+
+LOGGER: Logger = getLogger(__package__)
+
+NAME = "Healthbox "
+DOMAIN = "healthbox"
+VERSION = "0.0.1"
+MANUFACTURER = "Renson"
+ATTRIBUTION = ""
+SCAN_INTERVAL = timedelta(seconds=5)
+
+PLATFORMS = [Platform.SENSOR, Platform.BINARY_SENSOR]
+
+SERVICE_CHANGE_ROOM_PROFILE = "change_room_profile"
+SERVICE_CHANGE_ROOM_PROFILE_SCHEMA = vol.Schema(
+    {
+        vol.Required(cv.CONF_DEVICE_ID): cv.string,
+        vol.Required("profile_name"): cv.string
+    }
+)
+
+SERVICE_START_ROOM_BOOST = "start_room_boost"
+SERVICE_START_ROOM_BOOST_SCHEMA = vol.Schema(
+    {
+        vol.Required(cv.CONF_DEVICE_ID): cv.string,
+        vol.Required("boost_level"): vol.All(int, vol.Range(min=10, max=200)),
+        vol.Required("boost_timeout"): vol.All(int, vol.Range(min=5, max=720)),
+    }
+)
+
+SERVICE_STOP_ROOM_BOOST = "stop_room_boost"
+SERVICE_STOP_ROOM_BOOST_SCHEMA = vol.Schema(
+    {vol.Required("device_id"): cv.string},
+)
+
+ALL_SERVICES = [
+    SERVICE_START_ROOM_BOOST,
+    SERVICE_STOP_ROOM_BOOST,
+    SERVICE_CHANGE_ROOM_PROFILE
+]
+
+
+class HealthboxRoomBoost:
+    """Healthbox  Room Boost object."""
+
+    level: float
+    enabled: bool
+    remaining: int
+
+    def __init__(
+        self, level: float = 100, enabled: bool = False, remaining: int = 900
+    ) -> None:
+        """Initialize the HB Room Boost."""
+        self.level = level
+        self.enabled = enabled
+        self.remaining = remaining
+
+
+class HealthboxRoom:
+    """Healthbox  Room object."""
+
+    boost: HealthboxRoomBoost = None
+
+    def __init__(self, room_id: int, room_data: object) -> None:
+        """Initialize the HB Room."""
+        self.room_id: int = room_id
+        self.name: str = room_data["name"]
+        self.type: str = room_data["type"]
+        self.sensors_data: list = room_data["sensor"]
+        self.room_type: str = room_data["type"]
+
+    @property
+    def indoor_temperature(self) -> Decimal:
+        """HB Indoor Temperature."""
+        for sensor in self.sensors_data:
+            param = sensor.get("parameter")
+            if isinstance(param, dict) and "temperature" in param:
+                temp = param.get("temperature")
+                if isinstance(temp, dict) and "value" in temp:
+                    return temp["value"]
+        return None
+
+    @property
+    def indoor_humidity(self) -> Decimal:
+        """HB Indoor Humidity."""
+        for sensor in self.sensors_data:
+            param = sensor.get("parameter")
+            if isinstance(param, dict) and "humidity" in param:
+                hum = param.get("humidity")
+                if isinstance(hum, dict) and "value" in hum:
+                    return hum["value"]
+        return None
+
+    @property
+    def indoor_co2_concentration(self) -> Decimal | None:
+        """HB Indoor CO2 Concentration."""
+        for sensor in self.sensors_data:
+            param = sensor.get("parameter")
+            if isinstance(param, dict) and "concentration" in param:
+                conc = param.get("concentration")
+                if isinstance(conc, dict) and "value" in conc:
+                    return conc["value"]
+        return None
+
+    @property
+    def indoor_aqi(self) -> Decimal | None:
+        """HB Indoor Air Quality Index."""
+        for sensor in self.sensors_data:
+            param = sensor.get("parameter")
+            if isinstance(param, dict) and "index" in param:
+                idx = param.get("index")
+                if isinstance(idx, dict) and "value" in idx:
+                    return idx["value"]
+        return None
+
+
+class HealthboxDataObject:
+    """Healthbox Data Object."""
+
+    serial: str
+    description: str
+    warranty_number: str
+
+    global_aqi: float = None
+
+    rooms: list[HealthboxRoom]
+
+    def __init__(self, data: any) -> None:
+        """Initialize."""
+        self.serial = data["serial"]
+        self.description = data["description"]
+        self.warranty_number = data["warranty_number"]
+
+        self.global_aqi = self._get_global_aqi_from_data(data)
+
+        hb_rooms: list[HealthboxRoom] = []
+        for room in data["room"]:
+            hb_room = HealthboxRoom(room, data["room"][room])
+            hb_rooms.append(hb_room)
+
+        self.rooms = hb_rooms
+
+    def _get_global_aqi_from_data(self, data: any) -> float | None:
+        """Set Global AQI from Data Object."""
+        sensors = data.get("sensor") or []
+        for sensor in sensors:
+            if sensor.get("type") == "global air quality index":
+                param = sensor.get("parameter")
+                if isinstance(param, dict) and "index" in param:
+                    idx = param.get("index")
+                    if isinstance(idx, dict) and "value" in idx:
+                        return idx["value"]
+        return None
